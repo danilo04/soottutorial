8d5fbb1f0517ee6e94ae01376edbec2a254ed1f6
Danilo Dominguez Perez <danilo04@danilo0.(none)>
2014-09-17 17:31:15 -0500

    Initial import

---
+++ README.md
@@ -0,0 +1,17 @@
+##Deployment 
+
+###Requirements:
+  - java 6 (1.6) or java 7 (1.7)
+  - ant
+  - git
+  - eclipse
+  - xdot
+
+###Installation
+1. Run bash script
+2. Add soot-{branch}.jar to the libs folder and add it to the classpath of the project in Eclipse
+
+###Tutorial 1
+1. Create class edu.iastate.coms641.Tutorial1. Pick public static void main(String[] args)
+
+
---
+++ src/edu/iastate/coms641/IntegerComparator.java
@@ -0,0 +1,22 @@
+package edu.iastate.coms641;
+
+import java.util.Comparator;
+import java.util.Map;
+
+public class IntegerComparator implements Comparator<String> {
+	Map<String, Integer> base;
+	
+	public IntegerComparator(Map<String, Integer> base) {
+		this.base = base;
+	}
+	
+	@Override
+	public int compare(String o1, String o2) {
+		if (base.get(o1) >= base.get(o2)) {
+			return -1;
+		}
+		
+		return 1;
+	}
+
+}
